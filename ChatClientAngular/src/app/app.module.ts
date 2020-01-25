2c078f178e0e111891336c0cef4fa631e3178f38
Felix <felixbogi@gmail.com>
2020-01-25 14:21:52 +0100

    LoginScreen Design finished

--- ChatClientAngular/src/app/app.module.ts
+++ ChatClientAngular/src/app/app.module.ts
@@ -6,14 +6,20 @@ import { MaterialModule } from "./material.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LoginScreenComponent } from "./login-screen/login-screen.component";
+import { MainPageComponent } from "./main-page/main-page.component";
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [{ path: "main-page", component: MainPageComponent }];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginScreenComponent, MainPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
